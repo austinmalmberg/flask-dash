1c74f7fd63e67d7963a6ff70b747e11bed13d140
Austin Malmberg <austin.malmberg@gmail.com>
2020-05-29 09:38:51 -0400

    Added Calendar API endpoints

--- routes/auth/google/__init__.py
+++ routes/auth/google/__init__.py
@@ -2,6 +2,7 @@ import os
 from google_auth_oauthlib.flow import Flow
 
 api_endpoints = {
+    # USER INFO -- GET
     # {
     #   "id": <number string>,
     #   "email": <string>,
@@ -12,10 +13,30 @@ api_endpoints = {
     #   "picture": <url>
     #   "locale": "en"
     # }
-    'userinfo': 'https://www.googleapis.com/oauth2/v2/userinfo',
+    'userinfo': 'https://www.googleapis.com/userinfo/v2/me',
 
+    # Documentation -- https://developers.google.com/calendar/v3/reference/calendarList/list
+    'calendar_list': 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+
+    # Documentation -- https://developers.google.com/calendar/v3/reference/events/list
+    'calendar_events': lambda calendar_id: f'https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events',
+
+    # Documentation -- https://developers.google.com/calendar/v3/reference/colors/get
+    'calendar_colors': 'https://www.googleapis.com/calendar/v3/colors',
+
+    # Documentation -- https://developers.google.com/calendar/v3/reference/events/watch
+    'calendar_watch': lambda calendar_id: f'https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events/watch',
+
+    # Documentation -- https://developers.google.com/calendar/v3/reference/settings/list
+    'calendar_settings': 'https://www.googleapis.com/calendar/v3/users/me/settings',
+
+    # for requesting/refreshing tokens
     'oauth_token': 'https://oauth2.googleapis.com/token',
+
+    # for revoking tokens
     'oauth_token_revoke': 'https://oauth2.googleapis.com/revoke',
+
+    # for authorizing an application from a different device
     'limited_input_device_code': 'https://oauth2.googleapis.com/device/code'
 }
 
