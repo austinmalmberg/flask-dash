f15a02f0172626e8cb2d41e02f4a25009ff2d8a9
Austin Malmberg <austin.malmberg@gmail.com>
2020-07-01 20:01:22 -0400

    Added colors; Events are now parsed for before they are returned

--- helpers/google/calendars.py
+++ helpers/google/calendars.py
@@ -1,9 +1,9 @@
 from datetime import datetime, date, timedelta
-import pytz
 
 from googleapiclient.discovery import build
 
 from helpers.event_sort import sort_events
+
 
 # TODO: Add option to fail silently if credentials are invalid or _getservice fails
 # Right now, any problem getting the service or executing the action will throw an error or pass along an error
@@ -47,25 +47,59 @@ def get_calendar_settings(credentials):
         if page_token is None:
             break
 
-    return { setting['id']: setting['value'] for setting in settings }
+    return {setting['id']: setting['value'] for setting in settings}
 
 
-def get_events(credentials, calendar_id, date_min=date.today(), range=7):
+def get_colors(credentials):
+    service = _getservice(credentials)
+
+    colors = service.colors().get().execute()
+    return colors
+
+
+def _event_parser(event, colors=None):
+    colorId = event.get('colorId')
+
+    res = dict(
+        id=event['id'],
+        start=event['start'],
+        end=event['end'],
+        summary=event.get('summary', '(No title)'),
+        colorId=colorId,
+        htmlLink=event['htmlLink']
+    )
+
+    if colors and colorId:
+        res['background'] = colors['event'][colorId].get('background')
+        res['foreground'] = colors['event'][colorId].get('foreground')
+    elif colors and not colorId and '#holiday' not in event['organizer'].get('email', ''):
+        res['background'] = '#4285f4'
+        res['foreground'] = '#fff'
+    else:
+        res['background'] = '#1d1d1d'
+        res['foreground'] = '#fff'
+
+    return res
+
+
+def get_events(credentials, calendar_id, colors=None, date_min=date.today(), max_days=7):
     """
     Get events of the given calendar. Events are ordered by start time.  If no time range is specified, the time range
-    will be set as UTC midnight - 6 days later.
+    will be set as UTC midnight through 6 days later at 11:59.59 PM.
 
     :param credentials: OAuth 2.0 credentials
     :param calendar_id: The calendar id
     :param date_min: The min date range to get events
-    :param range: The date range to query the Google Calendar API for events. range=1 will get events from midnight to
+    :param max_days: The date range to query the Google Calendar API for events. range=1 will get events from midnight to
         11:59:59 of the same day
     :return: A list of events, ordered by start time
     """
+    if not colors:
+        colors = get_colors(credentials)
 
     # add 6 days to it
     time_min = datetime.combine(date_min, datetime.min.time())
-    time_max = time_min + timedelta(days=range, seconds=-1)
+    time_max = time_min + timedelta(days=max_days, seconds=-1)
 
     options = {
         'timeMin': f'{str(time_min.isoformat())}Z',
@@ -77,11 +111,11 @@ def get_events(credentials, calendar_id, date_min=date.today(), range=7):
     service = _getservice(credentials)
 
     event_list = []
-
     page_token = None
     while True:
         events = service.events().list(calendarId=calendar_id, **options).execute()
-        event_list += events['items']
+        for event in events['items']:
+            event_list.append(_event_parser(event, colors=colors))
         page_token = events.get('nextPageToken')
         if page_token is None:
             break
@@ -89,29 +123,21 @@ def get_events(credentials, calendar_id, date_min=date.today(), range=7):
     return event_list
 
 
-def get_event_start_dt(event, tz_str):
-    start = event['start'].get('dateTime')
-    if start:
-        return datetime.fromisoformat(start)
-
-    start = event['start'].get('date')
-    tz = pytz.timezone(tz_str)
-    return tz.fromutc(datetime.fromisoformat(start))
-
-
-def get_events_from_multiple_calendars(credentials, calendar_ids, date_min=None, time_range=None):
-    tz_str = 'America/New_York'
-
+def get_events_from_multiple_calendars(credentials, calendar_ids, date_min=None, max_days=None):
     def event_comparator(event1, event2):
-        d1 = get_event_start_dt(event1, tz_str)
-        d2 = get_event_start_dt(event2, tz_str)
+        # set dates to value from start.dateTime or start.date (in the case of all/multi-day events)
+        d1 = datetime.fromisoformat(
+            event1['start'].get('dateTime', event1['start'].get('date'))
+        )
 
-        if d1 == d2:
+        d2 = datetime.fromisoformat(
+            event2['start'].get('dateTime', event2['start'].get('date'))
+        )
+
+        if d1.timestamp() == d2.timestamp():
             return 0
-        elif d1 < d2:
-            return -1
 
-        return 1
+        return -1 if d1.timestamp() < d2.timestamp() else 1
 
     kwargs = dict(
         credentials=credentials
@@ -120,8 +146,10 @@ def get_events_from_multiple_calendars(credentials, calendar_ids, date_min=None,
     if date_min:
         kwargs['date_min'] = date_min
 
-    if time_range:
-        kwargs['range'] = time_range
+    if max_days:
+        kwargs['max_days'] = max_days
+
+    kwargs['colors'] = get_colors(credentials)
 
     res = []
     for calendar_id in calendar_ids:
@@ -130,10 +158,3 @@ def get_events_from_multiple_calendars(credentials, calendar_ids, date_min=None,
         res = sort_events(event_comparator, res, event_list)
 
     return res
-
-
-def get_colors(credentials):
-    service = _getservice(credentials)
-
-    colors = service.colors().get().execute()
-    return colors
