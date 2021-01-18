04d7ee9373e185ff0e79f2f767cbf2deb06cf034
Austin Malmberg <austin.malmberg@gmail.com>
2021-01-17 23:49:38 -0500

    Removed test logging

--- daily_dashboard/static/scripts/cookieManager.mjs
+++ daily_dashboard/static/scripts/cookieManager.mjs
@@ -3,7 +3,6 @@ export function setCookie(cname, cvalue, exdays) {
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   var expires = "expires="+ d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  console.log(`${cname} cookie set to ${cvalue}`);
 }
 
 export function getCookie(cname) {
