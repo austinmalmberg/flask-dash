4fcf95c5485d5858cbc383bd4dbd425901061843
Austin Malmberg <--replace-all>
2021-03-10 08:00:56 -0500

    Refactored weather

---
+++ daily_dashboard/providers/openweathermap.py
@@ -0,0 +1,31 @@
+import os
+
+import requests
+
+from daily_dashboard.util.errors import BaseError
+
+BASE_ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall'
+EXCLUDE = ['minutely', 'hourly']
+ACCEPTABLE_UNITS = ['imperial', 'metric', 'standard']
+
+
+def request_weather(lat, lon, units='imperial'):
+
+    params = {
+        'lat': lat,
+        'lon': lon,
+        'units': units,
+        'exclude': ','.join(EXCLUDE),
+        'appid': os.environ['OWM_API_KEY']
+    }
+
+    response = requests.get(BASE_ENDPOINT, params=params)
+
+    if response.status_code != 200:
+        raise BaseError(
+            status=504,
+            title='Weather Service Error',
+            message='There was a problem retrieving the weather for your location. Please try again later.'
+        )
+
+    return response.json()
--- daily_dashboard/routes/weather.py
+++ daily_dashboard/routes/weather.py
@@ -13,61 +13,81 @@ TODO: Allow user to update their preferred weather units through `/settings`
 
 """
 
-import os
-import requests
+from flask import Blueprint, request, session, jsonify, render_template, flash
+from flask_login import login_required
 
-from flask import Blueprint, request, session, jsonify, render_template
-from flask_login import login_required, current_user
-
+from daily_dashboard.helpers.location_manager import use_location_cookie
+from daily_dashboard.providers.openweathermap import request_weather, ACCEPTABLE_UNITS
 from daily_dashboard.dto.weather_dto import WeatherDto, CurrentWeatherDto
 from daily_dashboard.util.errors import BaseError
 
 bp = Blueprint('weather', __name__, url_prefix='/weather')
 
-BASE_ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall'
-EXCLUDE = ['minutely', 'hourly']
-# ACCEPTABLE_UNITS = {'standard', 'imperial', 'metric'}
+
+@bp.route('/', methods=('GET',))
+@login_required
+@use_location_cookie
+def get_weather():
+    json_response = request.args.get('res', None) == 'json'
+
+    kwargs = dict()
+    units = request.args.get('units', None)
+    if units and units in ACCEPTABLE_UNITS:
+        kwargs['units'] = units
+    elif 'weather_units' in session:
+        kwargs['units'] = session['weather_units']
+
+    try:
+        weather = request_weather(session['lat'], session['lon'], **kwargs)
+    except BaseError as err:
+        flash(err.message)
+
+        return (err.as_json() if json_response else err.as_template()), err.status
+
+    # only need 7 day forecast. This API returns 8
+    forecasts = weather['daily'][:7]
+    weather_dtos = []
+
+    for i, forecast in enumerate(forecasts):
+        dto = CurrentWeatherDto(forecast, weather['current']) if i == 0 else WeatherDto(forecast)
+        weather_dtos.append(dto)
+
+    if json_response:
+        return jsonify([dto.__dict__ for dto in weather_dtos])
+
+    # res_type == 'template' so create weather dtos from weather data
+    return render_template('components/forecast.html', weather_dtos=weather_dtos)
 
 
 @bp.route('/<float_neg:lat>,<float_neg:lon>', methods=('GET',))
 @login_required
-def fetch(lat, lon):
+def get_weather_from_coords(lat, lon):
+    json_response = request.args.get('res', None) == 'json'
+
+    kwargs = dict()
     units = request.args.get('units', None)
-    if units is None:
-        if 'weather_units' not in session:
-            session['weather_units'] = 'imperial' if current_user.locale == 'en' else 'metric'
-        units = session['weather_units']
+    if units and units in ACCEPTABLE_UNITS:
+        kwargs['units'] = units
+    elif 'weather_units' in session:
+        kwargs['units'] = session['weather_units']
 
-    params = {
-        'lat': lat,
-        'lon': lon,
-        'exclude': ','.join(EXCLUDE),
-        'units': units,
-        'appid': os.environ['OWM_API_KEY']
-    }
+    try:
+        weather = request_weather(lat, lon, **kwargs)
+    except BaseError as err:
+        flash(err.message)
 
-    response = requests.get(BASE_ENDPOINT, params=params)
-
-    as_json_response = request.args.get('res', None) == 'json'
-
-    if response.status_code != 200:
-        error = BaseError(
-            status=504,
-            title='Weather Service Error',
-            message='There was a problem retrieving the weather for your location. Please try again later.'
-        )
-        return (error.as_json() if as_json_response else error.as_template()), error.status
-
-    data = response.json()
-    if as_json_response:
-        return jsonify(data)
+        return (err.as_json() if json_response else err.as_template()), err.status
 
     # only need 7 day forecast. This API returns 8
-    forecasts = data['daily'][:7]
+    forecasts = weather['daily'][:7]
     weather_dtos = []
 
     for i, forecast in enumerate(forecasts):
-        dto = CurrentWeatherDto(forecast, data['current']) if i == 0 else WeatherDto(forecast)
+        dto = CurrentWeatherDto(forecast, weather['current']) if i == 0 else WeatherDto(forecast)
         weather_dtos.append(dto)
 
+    if json_response:
+        return jsonify([dto.__dict__ for dto in weather_dtos])
+
+    # res_type == 'template' so create weather dtos from weather data
     return render_template('components/forecast.html', weather_dtos=weather_dtos)
